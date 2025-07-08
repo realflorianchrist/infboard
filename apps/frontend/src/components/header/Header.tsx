c5ca570933ab142610947eea24bed2108a45e25b
Florian Christ <florian.christ@students.fhnw.ch>
2025-07-08 13:42:36 +0200

    add searchbar transition

--- apps/frontend/src/components/header/Header.tsx
+++ apps/frontend/src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import {IoIosPerson} from "react-icons/io";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between w-full p-4">
+        <header className={'flex items-center justify-between w-full p-4'}>
             <Link
                 href={Routes.HOME}
                 className="text-2xl font-bold text-accent"
@@ -14,8 +14,8 @@ export default function Header() {
                 infboard.ch
             </Link>
 
-            <div className="absolute left-1/2 -translate-x-1/2">
-                <Navigation />
+            <div className={'absolute left-1/2 -translate-x-1/2'}>
+                <Navigation/>
             </div>
 
             <div className={'flex items-center justify-center gap-4'}>
--- apps/frontend/src/components/header/Navigation.tsx
+++ apps/frontend/src/components/header/Navigation.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import {usePathname} from "next/navigation";
 import {Separator} from "@workspace/ui/components/separator";
 import SearchBar from "@/src/components/header/SearchBar";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Navigation() {
     const path = usePathname();
@@ -12,33 +12,45 @@ export default function Navigation() {
     const [isSearching, setIsSearching] = useState(false);
 
     return (
-        <nav className={'flex gap-4 items-center h-5'}>
-            {isSearching ? (
+        <nav className="flex gap-4 items-center h-5 relative">
+            <div
+                className={`flex items-center gap-4 transition-all duration-300 ${
+                    isSearching ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'
+                }`}
+            >
+                <Link
+                    href={Routes.HOME}
+                    className={path === Routes.HOME ? 'text-accent' : ''}
+                >
+                    Daten
+                </Link>
+                <Separator orientation="vertical"/>
+                <Link
+                    href={Routes.DASHBOARD}
+                    className={path === Routes.DASHBOARD ? 'text-accent' : ''}
+                >
+                    Dashboard
+                </Link>
+                <Separator orientation="vertical"/>
+                <div
+                    onClick={() => setIsSearching(true)}
+                    className={`cursor-pointer ${path === Routes.SEARCH ? 'text-accent' : ''}`}
+                >
+                    Suchen
+                </div>
+            </div>
+
+            <div
+                className={`
+                    absolute right-0
+                    overflow-hidden
+                    transition-all duration-700
+                    ${isSearching ? 'w-full opacity-100 pointer-events-auto' : 'w-0 opacity-0 pointer-events-none'}
+                  `}
+            >
                 <SearchBar setIsSearching={setIsSearching}/>
-            ) : (
-                <>
-                    <Link
-                        href={Routes.HOME}
-                        className={path === Routes.HOME ? 'text-accent' : ''}
-                    >
-                        Daten
-                    </Link>
-                    <Separator orientation="vertical"/>
-                    <Link
-                        href={Routes.DASHBOARD}
-                        className={path === Routes.DASHBOARD ? 'text-accent' : ''}
-                    >
-                        Dashboard
-                    </Link>
-                    <Separator orientation="vertical"/>
-                    <div
-                        onClick={()=>setIsSearching(!isSearching)}
-                        className={`cursor-pointer ${path === Routes.SEARCH ? 'text-accent' : ''}`}
-                    >
-                        Suchen
-                    </div>
-                </>
-            )}
+            </div>
         </nav>
+
     );
 }
