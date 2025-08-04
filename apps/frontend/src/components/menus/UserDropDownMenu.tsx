2871fb2bb170a39e68720a2e7ec74a161c6669f9
Florian Christ <florian.christ@students.fhnw.ch>
2025-08-04 12:14:10 +0200

    improve theme switch

--- apps/frontend/src/components/menus/UserDropDownMenu.tsx
+++ apps/frontend/src/components/menus/UserDropDownMenu.tsx
@@ -5,21 +5,26 @@ import {
     DropdownMenuLabel, DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "@workspace/ui/components/dropdown-menu";
-import {ReactNode} from "react";
+import {ReactNode, useRef} from "react";
 import ThemeSwitch from "@/src/components/ThemeSwitch";
 import {useLogout} from "@/src/api/hooks/api_hooks/authHooks";
 
 export default function UserDropDownMenu({children}: { children: ReactNode }) {
     const logout = useLogout();
+    const switchRef = useRef<HTMLButtonElement>(null);
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
             <DropdownMenuContent align={'end'}>
                 <DropdownMenuLabel>Mein Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
+                <DropdownMenuSeparator/>
                 <DropdownMenuItem>Meine Uploads</DropdownMenuItem>
-                <DropdownMenuItem>Dark mode <ThemeSwitch/></DropdownMenuItem>
+                <DropdownMenuItem onClick={() => {
+                    switchRef.current?.click();
+                }}>
+                    Dark mode <ThemeSwitch ref={switchRef}/>
+                </DropdownMenuItem>
                 <DropdownMenuItem onClick={logout}>Abmelden</DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
