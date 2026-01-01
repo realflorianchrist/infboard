049d9f29f140baebd8aa62da3842f1b7a5c8ad84
Florian Christ <florian.christ@students.fhnw.ch>
2026-01-01 11:11:28 +0100

    update login form

--- apps/frontend/src/components/auth/LoginForm.tsx
+++ apps/frontend/src/components/auth/LoginForm.tsx
@@ -14,6 +14,7 @@ import {successMessage} from "@/src/utils/getSuccessMessage";
 import AuthForm from "@/src/components/auth/form/AuthForm";
 import FormItem from "@/src/components/auth/form/FormItem";
 import PasswordInputField from "@/src/components/auth/form/PasswordInputField";
+import Loader from "@/src/components/loader/Loader";
 
 
 export default function LoginForm() {
@@ -33,7 +34,7 @@ export default function LoginForm() {
         loginMutation.mutate({user}, {
             onSuccess: () => {
                 toast.success(successMessage.LOGIN_SUCCESSFUL);
-                window.location.replace(routes.HOME);
+                router.push(routes.HOME);
             },
             onError: (e) => {
                 if (e.errorType === ErrorType.VALIDATION_ERROR) {
@@ -64,55 +65,59 @@ export default function LoginForm() {
     }
 
     return (
-        <AuthForm
-            onSubmit={handleLogin}
-        >
-            <FormItem>
-                <Label htmlFor='username'>Username / E-Mail</Label>
-                <Input
-                    id='username'
-                    required={true}
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </FormItem>
+        <>
+            {loginMutation.isPending && <Loader isFullScreen={true}/>}
 
-            <FormItem>
-                <Label htmlFor='password'>Passwort</Label>
-                <PasswordInputField
-                    id={'password'}
-                    required={true}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </FormItem>
+            <AuthForm
+                onSubmit={handleLogin}
+            >
+                <FormItem>
+                    <Label htmlFor='username'>Username / E-Mail</Label>
+                    <Input
+                        id='username'
+                        required={true}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </FormItem>
 
-            <div className={'flex flex-col items-start'}>
-                <Button
-                    variant={'link'}
-                    type={'button'}
-                    onClick={() => router.push(routes.RESET_PASSWORD)}
-                    className={'p-0'}
-                >
-                    Passwort vergessen?
+                <FormItem>
+                    <Label htmlFor='password'>Passwort</Label>
+                    <PasswordInputField
+                        id={'password'}
+                        required={true}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </FormItem>
+
+                <div className={'flex flex-col items-start'}>
+                    <Button
+                        variant={'link'}
+                        type={'button'}
+                        onClick={() => router.push(routes.RESET_PASSWORD)}
+                        className={'p-0'}
+                    >
+                        Passwort vergessen?
+                    </Button>
+                </div>
+
+                <Button type={'submit'}>
+                    Login
                 </Button>
-            </div>
 
-            <Button type={'submit'}>
-                Login
-            </Button>
-
-            <div className={'flex self-center items-center text-sm gap-3'}>
-                <p>Noch kein Account?</p>
-                <Button
-                    variant={'link'}
-                    type={'button'}
-                    onClick={() => router.push(routes.REGISTER)}
-                    className={'p-0'}
-                >
-                    Registrieren
-                </Button>
-            </div>
-        </AuthForm>
+                <div className={'flex self-center items-center text-sm gap-3'}>
+                    <p>Noch kein Account?</p>
+                    <Button
+                        variant={'link'}
+                        type={'button'}
+                        onClick={() => router.push(routes.REGISTER)}
+                        className={'p-0'}
+                    >
+                        Registrieren
+                    </Button>
+                </div>
+            </AuthForm>
+        </>
     );
 }
